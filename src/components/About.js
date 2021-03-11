8a08fd2018eca468d53d6314f2b4be67fd59f861
Johendri Haris Siregar <johendri.h.siregar@gmail.com>
2021-03-11 13:12:59 +0700

    2nd commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
+import logo from './img/johendri.jpg'
 
 const About = () => {
     return (
         <div>
+            <img src={logo} className='logo' alt=""/>
             <h4>Version 1.0.0</h4>
             <Link to="/">Go Back</Link>
         </div>
